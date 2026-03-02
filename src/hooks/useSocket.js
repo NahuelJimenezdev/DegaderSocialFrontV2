7aee31d9e770a80609586dee8a0c1b7dcb546534
Nahuel Jimenez <naedjima93@gmail.com>
2026-03-02 14:47:27 -0300

    actualizacion arena

---
+++ src/hooks/useSocket.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import { getSocket } from '../shared/lib/socket';
+
+export const useSocket = () => {
+  const [socket, setSocket] = useState(getSocket());
+
+  useEffect(() => {
+    // Si el socket se inicializa más tarde, lo capturamos
+    const currentSocket = getSocket();
+    if (currentSocket !== socket) {
+      setSocket(currentSocket);
+    }
+  }, []);
+
+  return socket;
+};
